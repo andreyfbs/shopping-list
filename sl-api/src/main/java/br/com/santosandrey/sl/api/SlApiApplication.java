70c70c44b782e9036948216f4fb3c3f7aadc2925
andreyfbs <andrey.barros.santos@gmail.com>
2017-05-06 19:31:57 -0300

    First Test Hello World of the Application

--- sl-api/src/main/java/br/com/santosandrey/sl/api/SlApiApplication.java
+++ sl-api/src/main/java/br/com/santosandrey/sl/api/SlApiApplication.java
@@ -2,8 +2,10 @@ package br.com.santosandrey.sl.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("br.com.santosandrey.sl")
 public class SlApiApplication {
 
     public static void main(String[] args) {
